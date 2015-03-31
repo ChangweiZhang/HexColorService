193de88de8f8fac297ebcdb7d42211d2c35a5e57
Changwei Zhang <z18205051832@hotmail.com>
2015-03-31 11:02:24 +0800

    Update CWService.h

--- CWService/CWService.h
+++ CWService/CWService.h
@@ -2,8 +2,6 @@
 //  library.h
 //  library
 //
-//  Created by ZhangChangwei on 15/3/26.
-//  Copyright (c) 2015年 Shanghai Baogaitou Enterprise Development Co., Ltd. All rights reserved.
 //
 
 #import "UIColor+HexColor.h"
